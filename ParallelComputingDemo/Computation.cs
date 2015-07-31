1f17f6ec43a3c43a17702cf522456a01033ca603
TF <torben.foersterling@gmail.com>
2015-07-31 17:46:50 +0200

    Init

---
+++ ParallelComputingDemo/Computation.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ParallelComputingDemo
+{
+    class Computation
+    {
+        public static ConcurrentDictionary<int, int> GetAmicableNumbersParallelized(int n)
+        {
+            ConcurrentDictionary<int, int> amicableNumbers =
+                new ConcurrentDictionary<int, int>();
+
+            Parallel.For(1, n, (i) =>
+            {
+                Parallel.For(i, n, (j) =>
+                {
+                    if (i != j &&
+                        i == GetEvenDivisors(j).Sum() &&
+                        j == GetEvenDivisors(i).Sum())
+                    {
+                        amicableNumbers.TryAdd(i, j);
+                        Console.WriteLine("Found {0} and {1} using Thread {2}.",
+                            GetEvenDivisors(i).Sum(),
+                            GetEvenDivisors(j).Sum(),
+                            Thread.CurrentThread.ManagedThreadId);
+                    }
+                }
+                );
+            });
+
+            return amicableNumbers;
+        }
+
+        public static Dictionary<int, int> GetAmicableNumbersSynchronized(int n)
+        {
+            Dictionary<int, int> amicableNumbers =
+                new Dictionary<int, int>();
+
+            for (int i = 1; i < n; i++)
+            {
+                for (int j = i; j < n; j++)
+                {
+                    if (i != j &&
+                        i == GetEvenDivisors(j).Sum() &&
+                        j == GetEvenDivisors(i).Sum())
+                    {
+                        amicableNumbers.Add(i, j);
+                        Console.WriteLine("Found {0} and {1} using Thread {2}.",
+                            GetEvenDivisors(i).Sum(),
+                            GetEvenDivisors(j).Sum(),
+                            Thread.CurrentThread.ManagedThreadId);
+                    }
+                }
+            }
+            return amicableNumbers;
+        }
+
+        private static List<int> GetEvenDivisors(int n)
+        {
+            List<int> divisors = new List<int>();
+            for (int i = 1; i < n; i++)
+            {
+                if (n % i == 0)
+                {
+                    divisors.Add(i);
+                }
+            }
+            return divisors;
+        }
+
+    }
+}
---
+++ ParallelComputingDemo/DemoApplication.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParallelComputingDemo
+{
+    class DemoApplication
+    {
+        static void Main()
+        {
+            Console.WriteLine("--- CPU parallel processing test ---\n");
+            Console.WriteLine("Enter number to search all amicable numbers in: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Stopwatch stopWatch = new Stopwatch();
+            stopWatch.Start();
+
+            Task parallelTask = new Task(delegate
+            {
+                var res = Computation.GetAmicableNumbersParallelized(n);
+            });
+            parallelTask.Start();
+            parallelTask.Wait();
+            stopWatch.Stop();
+            var ts = stopWatch.Elapsed;
+            var elapsedTime = String.Format("{0:00}:{1:00}.{2:00}",
+                ts.Minutes, ts.Seconds,
+                ts.Milliseconds / 10);
+            Console.WriteLine("Runtime of parallel computation: {0} minutes.\n",
+                elapsedTime);
+
+            stopWatch.Reset();
+            stopWatch.Start();
+
+            Task synchronizedTask = new Task(delegate
+            {
+                var res = Computation.GetAmicableNumbersSynchronized(n);
+            });
+            synchronizedTask.Start();
+            synchronizedTask.Wait();
+            stopWatch.Stop();
+            ts = stopWatch.Elapsed;
+            elapsedTime = String.Format("{0:00}:{1:00}.{2:00}",
+                ts.Minutes, ts.Seconds,
+                ts.Milliseconds / 10);
+      
+            Console.WriteLine("Runtime of synchronous computation: {0} minutes.\n",
+                elapsedTime);
+
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
